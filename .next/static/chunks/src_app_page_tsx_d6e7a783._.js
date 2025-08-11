d5bc918487629bd1e108f53c396235a078b6a379
shivam <shivamkaship3@gmail.com>
2025-08-11 20:25:42 +0530

    Add .next/static/chunks/src_app_page_tsx_d6e7a783._.js 1754924136545

---
+++ .next/static/chunks/src_app_page_tsx_d6e7a783._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_58dbb76d._.js",
+  "static/chunks/node_modules_51094644._.js"
+],
+    source: "dynamic"
+});
